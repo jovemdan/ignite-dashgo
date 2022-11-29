25301e424f9d5fa6a8461036d8aa64aedcd49f93
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-11-29 18:45:07 -0300

    feat: Placing components in the file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,29 @@
-
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail"></Input>
+          <Input name="password" type="password" label="E-mail"></Input>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme={'pink'} size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
