a89e80c42a2b6535f4cd7a84ebf6b7783719e4fe
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-12-20 19:07:43 -0300

    feat: Responsividade Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Danilo Gonçalves</Text>
-        <Text color="gray.300">danilogdev@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Danilo Gonçalves</Text>
+          <Text color="gray.300">danilogdev@gmail.com</Text>
+        </Box>
+      )}
+
 
       <Avatar size="md" name="Danilo Gonçalves" src="https://github.com/jovemdan.png" />
     </Flex>
