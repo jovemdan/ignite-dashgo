99182b9f133187082decfd855cea60c2fd8f8975
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-12-26 23:17:47 -0300

    feat: Responsive Table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,116 +1,153 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+import Link from 'next/link'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { SideBar } from '../../components/Sidebar'
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Box>
       <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px={['4', '4', '6']}>
         <SideBar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8" >
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+            </Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo
-            </Button>
+            <Link href='users/create' passHref>
+              <Button
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
+          <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
+                <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                  <Checkbox colorScheme='pink' />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                <Th width='8'></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight="bold">Danilo Menezes</Text>
-                    <Text fontSize="sm" color="gray.300">danilogdev@gmail.com</Text>
+                    <Text fontWeight='bold'>Danilo Menezes</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      danilogdev@gmail.com
+                    </Text>
                   </Box>
                 </Td>
+                {isWideVersion && <Td>08 de Dezembro, 2022</Td>}
                 <Td>
-                  08 de Dezembro, 2022
-                </Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
+                  {isWideVersion ? (
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='sm'
+                      colorScheme='purple'
+                      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                    >
+                      Editar
+                    </Button>
+                  ) : (
+                    ''
+                  )}
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight="bold">Danilo Menezes</Text>
-                    <Text fontSize="sm" color="gray.300">danilogdev@gmail.com</Text>
+                    <Text fontWeight='bold'>Danilo Menezes</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      danilogdev@gmail.com
+                    </Text>
                   </Box>
                 </Td>
+                {isWideVersion && <Td>08 de Dezembro, 2022</Td>}
                 <Td>
-                  08 de Dezembro, 2022
-                </Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
+                  {isWideVersion ? (
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='sm'
+                      colorScheme='purple'
+                      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                    >
+                      Editar
+                    </Button>
+                  ) : (
+                    ''
+                  )}
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight="bold">Danilo Menezes</Text>
-                    <Text fontSize="sm" color="gray.300">danilogdev@gmail.com</Text>
+                    <Text fontWeight='bold'>Danilo Menezes</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      danilogdev@gmail.com
+                    </Text>
                   </Box>
                 </Td>
+                {isWideVersion && <Td>08 de Dezembro, 2022</Td>}
                 <Td>
-                  08 de Dezembro, 2022
-                </Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
+                  {isWideVersion ? (
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='sm'
+                      colorScheme='purple'
+                      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                    >
+                      Editar
+                    </Button>
+                  ) : (
+                    ''
+                  )}
                 </Td>
               </Tr>
             </Tbody>
