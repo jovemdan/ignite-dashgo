e20282ac74ed6cbdc711e0a45549cfa64a1f37aa
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-12-26 23:18:13 -0300

    feat: Responsive CreateUser

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from '@chakra-ui/react'
+import Link from 'next/link'
 import { Input } from '../../components/Form/Input'
 import { Header } from '../../components/Header'
 import { SideBar } from '../../components/Sidebar'
@@ -7,32 +17,40 @@ export default function CreateUser() {
   return (
     <Box>
       <Header />
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <SideBar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8" >
-          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
-          <Divider my="6" borderColor="gray.700" />
+        <Box flex='1' borderRadius={8} bg='gray.800' p={['6', '8']}>
+          <Heading size='lg' fontWeight='normal'>
+            Criar usuário
+          </Heading>
+          <Divider my='6' borderColor='gray.700' />
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" type="email" label="E-mail" />
+          <VStack spacing='8'>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='name' label='Nome completo' />
+              <Input name='email' type='email' label='E-mail' />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='password' type='password' label='Senha' />
+              <Input
+                name='password_confirmation'
+                type='password'
+                label='Confirmação da senha'
+              />
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
-            <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
-              <Button colorScheme="pink">Salvar</Button>
-
+          <Flex mt='8' justify='flex-end'>
+            <HStack spacing='4'>
+              <Link href='users/create' passHref>
+                <Button as='a' colorScheme='whiteAlpha'>
+                  Cancelar
+                </Button>
+              </Link>
+              <Button colorScheme='pink'>Salvar</Button>
             </HStack>
-
           </Flex>
         </Box>
       </Flex>
