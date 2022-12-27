5eb9e8c31e53dcfaf24c1f3fb37c894841ef06a8
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-12-26 23:19:15 -0300

    fix: Rename label to "senha"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,24 @@ import { Button, Flex, Stack } from '@chakra-ui/react'
 import { Input } from '../components/Form/Input'
 export default function Home() {
   return (
-    <Flex
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-    >
+    <Flex w='100vw' h='100vh' align='center' justify='center'>
       <Flex
-        as="form"
-        width="100%"
+        as='form'
+        width='100%'
         maxWidth={360}
-        bg="gray.800"
-        p="8"
+        bg='gray.800'
+        p='8'
         borderRadius={8}
-        flexDir="column"
+        flexDir='column'
       >
         <Stack spacing={4}>
-          <Input name="email" type="email" label="E-mail"></Input>
-          <Input name="password" type="password" label="E-mail"></Input>
+          <Input name='email' type='email' label='E-mail'></Input>
+          <Input name='password' type='password' label='Senha'></Input>
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme={'pink'} size="lg">Entrar</Button>
+        <Button type='submit' mt='6' colorScheme={'pink'} size='lg'>
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
